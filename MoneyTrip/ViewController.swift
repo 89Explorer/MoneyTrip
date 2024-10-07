8a4474c65b8b43076a65d201be6c59a66441ac86
89Explorer <leral3@naver.com>
2024-10-07 14:30:15 +0900

    Initial Commit

---
+++ MoneyTrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTrip
+//
+//  Created by 권정근 on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoneyTripTests/MoneyTripTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTripTests.swift
+//  MoneyTripTests
+//
+//  Created by 권정근 on 10/7/24.
+//
+
+import Testing
+@testable import MoneyTrip
+
+struct MoneyTripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
